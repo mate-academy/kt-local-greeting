29357b7f28b9afaf6ae317bb6f771d817d72ae16
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2024-02-25 18:36:09 +0200

    Added task impl.

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -1,9 +1,22 @@
 package mate.academy
 
-fun greetUsers() {
-    // implement function
-}
+private const val STOP_STRING = ""
 
 fun main() {
     greetUsers()
+}
+
+fun greetUsers() {
+    fun printGreeting(name: String?) {println("Hello, $name!")}
+
+    fun askUserInput(): String? {
+        println("Please enter a name (or press Enter to quit):")
+        return readLine()
+    }
+
+    var name = askUserInput()
+    while (name != STOP_STRING) {
+        askUserInput()
+        printGreeting(name)
+    }
 }
