9fea74c7067e9aff277f18a17eafaf31c9965681
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2024-02-26 12:41:39 +0200

    Fixed task impl.

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -1,18 +1,18 @@
 package mate.academy
 
 fun greetUsers() {
-    fun askUserInput(): String? {
+    fun askUserInput(): String {
         println("Please enter a name (or press Enter to quit):")
-        return readline()
+        return readln()
     }
 
-    fun printGreeting(name: String?) {
+    fun printGreeting(name: String) {
         println("Hello, $name!")
 
     }
 
     var name = askUserInput()
-    while (name) {
+    while (name != "") {
         printGreeting(name)
         name = askUserInput()
     }
