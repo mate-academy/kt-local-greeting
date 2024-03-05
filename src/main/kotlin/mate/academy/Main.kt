fed655f74078859fdbf4aab01a0127a2f1fb3a9f
VolodymyrDavidov <volodymyrdavidov1@gmail.com>
2024-03-05 15:57:33 +0200

    done

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -1,7 +1,9 @@
 package mate.academy
 
 fun greetUsers() {
-    // implement function
+    println("Please enter a name (or press Enter to quit): ")
+    val name = readln()
+    println("Hello, $name!")
 }
 
 fun main() {
