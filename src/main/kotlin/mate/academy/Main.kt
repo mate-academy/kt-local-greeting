c606f99b8e737905f7134f4f904d43d7418c5541
Danil Ponomarenko <danil.ponomvrenko@gmail.com>
2024-03-07 17:53:07 +0200

    Created a local function that asks for a name and greets user & stops when we provide empty string

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -1,9 +1,18 @@
 package mate.academy
 
-fun greetUsers() {
-    // implement function
-}
-
 fun main() {
     greetUsers()
+}
+
+fun greetUsers() {
+    var flag = true
+    while (flag) {
+        println("Please enter a name (or press Enter to quit): ")
+        val name : String? = readLine()
+        if (name == "") {
+            flag = false
+            continue
+        }
+        println("Hello, $name!")
+    }
 }
