ae70c0bdf8baf20245c1eda44c5d5f7bf3e11def
VolodymyrDavidov <volodymyrdavidov1@gmail.com>
2024-03-07 13:42:40 +0200

    add checks for null and empty line

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -3,7 +3,11 @@ package mate.academy
 fun greetUsers() {
     println("Please enter a name (or press Enter to quit): ")
     val name = readln()
-    println("Hello, $name!")
+    if (name.isNullOrEmpty()) {
+        println("You didn't enter a valid name. Goodbye!")
+    } else {
+        println("Hello, $name!")
+    }
 }
 
 fun main() {
