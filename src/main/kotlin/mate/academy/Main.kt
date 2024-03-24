b6ca8c3f0765c693aa61b25078bb35c81b3bf1dc
Volodymyr Ozemblovskyi <Vladimir.ozem.ps@gmail.com>
2024-03-24 18:25:37 +0200

    upgraded implementation in fun greetUsers()

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -3,11 +3,10 @@ package mate.academy
 const val GREETING = "Please enter a name (or press Enter to quit): "
 
 fun greetUsers() {
-    var userName: String
     while (true) {
         println(GREETING)
-        userName = readlnOrNull().toString()
-        if (userName == "") break else println("Hello, $userName!")
+        val userName: String = readlnOrNull().toString().ifEmpty { return }
+        println("Hello, $userName!")
     }
 }
 
