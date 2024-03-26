9417411e2034c6da301fac45b91e649c0f1c2848
Denys Okhrymovych <denys.ohrymovych.2015@gmail.com>
2024-03-26 13:16:21 +0200

    implement the greetUsers() function

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -1,7 +1,14 @@
 package mate.academy
 
 fun greetUsers() {
-    // implement function
+    println("Please enter a name (or press Enter to quit):")
+    var name = readLine() ?: ""
+
+    while (name.isNotEmpty()) {
+        println("Hello, $name!")
+        println("Please enter a name (or press Enter to quit):")
+        name = readLine() ?: ""
+    }
 }
 
 fun main() {
