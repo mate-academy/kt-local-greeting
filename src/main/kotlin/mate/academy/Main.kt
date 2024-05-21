611e03b1d3873a3abb328c3dd597e214466b6376
Yuriy.Khilko <yurakhilko@gmail.com>
2024-05-21 15:13:45 +0300

    added realization of greetUsers()

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -1,7 +1,13 @@
 package mate.academy
 
 fun greetUsers() {
-    // implement function
+    println("Please enter a name (or press Enter to quit):")
+    val name: String? = readlnOrNull()
+    if (name == "")
+        return
+    else
+        println("Hello, $name!")
+    greetUsers()
 }
 
 fun main() {
