8f9a27926153e0a585606349457560cd183622c9
Nonzeq <bgdn.kobylchak@gmail.com>
2024-05-30 12:03:07 +0300

    implemented

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -1,7 +1,17 @@
 package mate.academy
 
+const val OPTION_MESSAGE: String = "Please enter a name (or press Enter to quit): "
+const val HELLO_MESSAGE: String = "Hello, "
+
 fun greetUsers() {
-    // implement function
+    var userInput: String
+    do {
+        print(OPTION_MESSAGE)
+        userInput = readln()
+        if (userInput.isNotEmpty()) {
+            println("$HELLO_MESSAGE $userInput!")
+        }
+    } while (userInput.isNotEmpty())
 }
 
 fun main() {
