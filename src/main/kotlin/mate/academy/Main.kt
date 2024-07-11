35b143ef15465054b98f5495019fdcd1da4cd058
azzilian <crusotdeath@gmail.com>
2024-07-12 00:01:15 +0300

    Main refactor - solution to task

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -1,7 +1,19 @@
 package mate.academy
 
+const val GREETING_TEXT = "Please enter a name (or press Enter to quit):"
+
 fun greetUsers() {
-    // implement function
+    fun greetingText() : String = GREETING_TEXT
+    fun readInput() : String? = readLine()
+    fun checkResponse (readInput : String?) {
+        if (!readInput.isNullOrBlank()) {
+            println("Hello, $readInput!")
+            greetUsers()
+        }
+    }
+
+    println(greetingText())
+    checkResponse(readInput())
 }
 
 fun main() {
