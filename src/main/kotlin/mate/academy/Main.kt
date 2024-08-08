b58191301ba9c6fad8a72e6518d5162ae7672e27
Ruslan Cherniavskyi <ruslan.od.ua@ukr.net>
2024-08-08 14:11:41 +0300

    added fun

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -1,7 +1,19 @@
 package mate.academy
 
 fun greetUsers() {
-    // implement function
+
+    fun askAndGreet() {
+        while (true) {
+            println("Please enter a name (or press Enter to quit):")
+            val name = readLine() ?: ""
+            if (name.isEmpty()) {
+                break
+            }
+            println("Hello, $name!")
+        }
+    }
+
+    askAndGreet()
 }
 
 fun main() {
