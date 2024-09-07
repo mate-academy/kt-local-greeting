3c23afafa629c3f91ffe2998bcf9759cd4659319
Oleksand Kolisnyk <kolisnykol@gmail.com>
2024-09-07 13:05:11 +0300

    kom_solution

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -2,6 +2,14 @@ package mate.academy
 
 fun greetUsers() {
     // implement function
+    var inputName = "";
+    do {
+        println("Please enter a name (or press Enter to quit):")
+        inputName = readln()
+        if (!inputName.isEmpty()) {
+            println("Hello, $inputName!")
+        }
+    } while (!inputName.isEmpty())
 }
 
 fun main() {
