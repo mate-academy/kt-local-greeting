d6396fdfc89393df6aa127aa51641823301b3385
Serhii Nazarenko <nasarenko82@gmail.com>
2024-09-10 21:06:25 -0600

    first commit

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -1,7 +1,15 @@
 package mate.academy
 
+const val TEXT = "Please enter a name (or press Enter to quit):"
+
 fun greetUsers() {
-    // implement function
+    var name: String?
+    do {
+        println(TEXT)
+        name = readLine()
+        if (!name.isNullOrEmpty()) println("Hello $name")
+    } while (!name.isNullOrEmpty())
+
 }
 
 fun main() {
