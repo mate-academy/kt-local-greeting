f926c0fef433dbc1885d9c2de5fa0d868da8c464
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2024-09-23 21:21:00 +0300

    Fixed a bug with using debugging.

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -2,10 +2,10 @@ package mate.academy
 
 fun greetUsers() {
     while (true) {
-        println("Please enter a name (or press Enter to quit")
-        var username : String? = readLine()
-        if (username == null) {
-            return;
+        println("Please enter a name (or press Enter to quit)")
+        var username : String? = readlnOrNull()
+        if (username == null || username == "") {
+            return
         } else {
             println("Hello, $username")
         }
