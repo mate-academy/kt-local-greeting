bb7cf65508121d93d79ac7908a2b418c4f6d0ea2
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2024-09-30 19:02:14 +0300

    Refactored the code

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -4,11 +4,7 @@ fun greetUsers() {
     while (true) {
         println("Please enter a name (or press Enter to quit)")
         var username : String? = readlnOrNull()
-        if (username == null || username == "") {
-            return
-        } else {
-            println("Hello, $username")
-        }
+        if (username == null || username == "") return else println("Hello, $username")
     }
 }
 
