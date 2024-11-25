c59de4fb5a39657772260fb4dc8c108ebbc6e8d7
Artem Klishch <artemklishch@gmail.com>
2024-11-25 10:38:46 +0200

    feat: func for greeting input

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -1,7 +1,11 @@
 package mate.academy
 
 fun greetUsers() {
-    // implement function
+    println("Please enter a name (or press Enter to quit):")
+    val userName = readlnOrNull()
+    if (!userName.isNullOrEmpty()) {
+        println("Hello, $userName!")
+    }
 }
 
 fun main() {
