d2287d38d521c4005d06450537e2a7f698e9023a
Anatolii Nazarov <anatoly_n@ukr.net>
2024-11-27 08:37:21 +0200

    For check

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -3,7 +3,7 @@ package mate.academy
 fun greetUsers() {
     // implement function
     print("Please enter a name (or press Enter to quit): ")
-    val name = readLine()
+    val name: String? = readLine()
     println("Hello, $name!")
 
 }
