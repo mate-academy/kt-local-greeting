a9c5bea4ff1acf4431fab8ba47b6f628b84c123e
Artem Makovskyy <photomak79@gmail.com>
2024-12-08 12:15:23 +0200

    done 2

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -1,14 +1,19 @@
 package mate.academy
 
 fun greetUsers() {
+
     while (true) {
+
         print("Please enter a name (or press Enter to quit): ")
         val name = readLine()
+
         if (name.isNullOrBlank()) {
             break
         }
+
         println("Hello, $name!")
     }
+
 }
 
 fun main() {
