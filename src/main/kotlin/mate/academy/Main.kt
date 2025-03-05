6e59c6271e7d209aefb537fe4f56341871ff843d
loranazarenko <loranazarenko@ukr.net>
2025-03-05 17:25:54 +0200

    First commit

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -3,9 +3,13 @@ package mate.academy
 fun greetUsers() {
     println("Please enter a name (or press Enter to quit): ")
     val name = readLine()
-    println("Hello, $name!")
+    if (name != "" && name != null)  {
+        println("Hello, $name!")
+    }
 }
 
 fun main() {
     greetUsers()
 }
+
+
