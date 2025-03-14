c3fe3bed2f98f465bd10c86c3b47132f95dc3654
Roman Voynahiy <romanvoynahiy@gmail.com>
2025-03-14 17:19:37 +0200

    Implemented greetUsers() function that prompts the user for a name and prints a greeting for each input.

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -1,7 +1,14 @@
 package mate.academy
 
 fun greetUsers() {
-    // implement function
+    while (true) {
+        println("Please enter a name (or press Enter to quit): ")
+        val usersName = readlnOrNull()
+        if (usersName.isNullOrEmpty()) {
+            break
+        }
+        println("Hello, $usersName!")
+    }
 }
 
 fun main() {
