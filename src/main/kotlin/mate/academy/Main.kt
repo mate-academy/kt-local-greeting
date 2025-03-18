f2dadb8245871c1e3485cd077e706df2e55df01b
aksoli666 <olexsandradorofeieva@gmail.com>
2025-03-18 15:10:06 +0100

    1.0 Done Local greeting

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -1,7 +1,9 @@
 package mate.academy
 
 fun greetUsers() {
-    // implement function
+    println("Please enter a name (or press Enter to quit):")
+    var name = readLine()
+    if (name == "") return else println("Hello, $name!")
 }
 
 fun main() {
