56d18ff0979bdae77c239052d481cb051bc2de32
Dima Davydov <Kilo6559@gmail.com>
2025-05-20 20:00:53 +0300

    Fixed

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -1,9 +1,19 @@
 package mate.academy
 
 fun greetUsers() {
-    println("Please enter a name (or press Enter to quit):")
-    val name = readLine()
-    println("Hello, $name!")
+    fun greet(name: String) {
+        println("Hello, $name!")
+    }
+    while (true) {
+        print("Please enter a name (or press Enter to quit): ")
+        val name = readLine()
+
+        if (name.isNullOrBlank()) {
+            break
+        }
+
+        greet(name)
+    }
 }
 
 fun main() {
