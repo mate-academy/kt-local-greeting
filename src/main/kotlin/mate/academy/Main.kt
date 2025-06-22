b1e647c03e2e8308bdce068957a31e753475de66
Oleksii Kolinko <138433107+OleksiiKolinko@users.noreply.github.com>
2025-06-22 02:24:51 +0200

    solution1

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -3,7 +3,10 @@ package mate.academy
 fun greetUsers() {
     while (true) {
         println("Please enter a name (or press Enter to quit):")
-        val name = readlnOrNull()?.trim()?.ifBlank { return }
+        val name = readlnOrNull()?.trim()
+        if (name.isNullOrBlank()) {
+            break
+        }
         println("Hello, $name!")
     }
 }
